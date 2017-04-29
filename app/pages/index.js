43eb55a1af33fff93a7e423ba0f4b5f8e5d6b271
Tom Wieland <tom.wieland@gmail.com>
2017-04-29 23:14:32 +0200

    got hot reloading working in docker compose again

---
+++ app/pages/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Layout from '../components/layout'
+import log from '../lib/log'
+
+export default class extends React.Component {
+  render() {
+    log.debug('pages/index#render')
+
+    const {
+      url: {
+        pathname,
+      },
+    } = this.props
+
+    log.debug('pages/index#render pathname', pathname)
+
+    return (
+      <Layout>
+        <Header pathname={pathname} />
+        ASDASDSADAS123123123
+        <Footer />
+      </Layout>
+    )
+  }
+}
