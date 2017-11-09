889197818c2c4a2b7b880ec9e7c4e05fb9250dba
Tom Wieland <tom.wieland@gmail.com>
2017-11-09 22:49:59 +0100

    using js-data orm

--- services/api/graphql/resolvers.js
+++ services/api/graphql/resolvers.js
@@ -1,7 +1,7 @@
 const { Kind } = require("graphql/language");
 
+const store = require('../lib/models/store');
 const candlestick = require("../lib/models/candlestick");
-const currency = require("../lib/models/currency");
 const user = require("../lib/models/user");
 
 const { isDate, isString, isNumber } = require('lodash');
@@ -17,21 +17,12 @@ const jsonresult = a => {
 };
 
 module.exports = {
-  /*
-  Currency: {
-  },
-  */
-
   Date: {
     __serialize: value => {
-      console.log('Date#__serialize', value)
-
       return value.getTime();
     },
 
     __parseValue: value => {
-      console.log('Date#__parseValue', value)
-
       if (isNumber(value)) {
         return value;
       }
@@ -44,8 +35,6 @@ module.exports = {
     },
 
     __parseLiteral: ast => {
-      console.log('Date#__parseLiteral', ast)
-
       if (ast.kind === Kind.INT) {
         return ast.value;
       }
@@ -54,11 +43,11 @@ module.exports = {
 
   Candlestick: {
     currencyA: ({ currencyA }) => {
-      return currency.getCurrencyById(currencyA)
+      return store.find('currency', currencyA)
     },
 
     currencyB: ({ currencyB }) => {
-      return currency.getCurrencyById(currencyB)
+      return store.find('currency', currencyB)
     },
   },
 
@@ -75,17 +64,107 @@ module.exports = {
       return user.getUserByEmail(email);
     },
 
-    currencies: () => {
-      return currency.getCurrencies();
+    currencies: async () => {
+      const queryResult = await store.findAll('currency');
+
+      if (queryResult && queryResult.length) {
+        return queryResult;
+      }
+
+      const queryURL = `https://poloniex.com/public?command=returnCurrencies`;
+
+      const apiResult = await fetch(queryURL)
+      const apiResultJSON = await apiResult.json()
+
+      if (apiResultJSON.error) {
+        throw new Error(apiResultJSON.error)
+      }
+
+      const keys = Object.keys(apiResultJSON);
+
+      const documents = Object.values(apiResultJSON).map((v, i) => {
+        delete v.id;
+        delete v.depositAddress;
+
+        v.key = keys[i];
+
+        v.frozen = !!v.frozen;
+        v.delisted = !!v.delisted;
+        v.disabled = !!v.disabled;
+
+        return v;
+      });
+
+      await store.destroyAll('currency');
+
+      const createResult = await store.createMany('currency', documents);
+
+      return createResult;
     },
 
     currencyById: (root, { id }) => {
       return currency.getCurrencyById(id);
     },
 
-    candlesticks: (root, { currencyA, currencyB, period, start, end }) => {
-      return candlestick.getCandlesticks(currencyA, currencyB, period, start, end);
-    }
+    candlesticks: async (root, { currencyA: currencyAKey, currencyB: currencyBKey, period, start, end }) => {
+      const currencyAResult = await store.findAll('currency', {
+        where: {
+          key: currencyAKey,
+        },
+      })
+
+      if (currencyAResult.length !== 1) {
+        throw new Error(`Invalid currency: ${currencyA}.`)
+      }
+
+      const [currencyA] = currencyAResult;
+
+      const currencyBResult = await store.findAll('currency', {
+        where: {
+          key: currencyBKey,
+        },
+      })
+
+      if (currencyBResult.length !== 1) {
+        throw new Error(`Invalid currency: ${currencyB}.`)
+      }
+
+      const [currencyB] = currencyBResult;
+
+      // Efficient between query.
+      const queryResult = await store.getAdapter("rethinkdb").r.table('candlesticks').between(start, end)
+
+      if (queryResult && queryResult.length) {
+        return queryResult;
+      }
+
+      const currencyPair = `${currencyA.key}_${currencyB.key}`;
+
+      const queryURL = `https://poloniex.com/public?command=returnChartData&currencyPair=${currencyPair}&start=${start}&end=${end}&period=${period}`;
+
+      const apiResult = await fetch(queryURL)
+      const apiResultJSON = await apiResult.json()
+
+      if (apiResultJSON.error) {
+        throw new Error(apiResultJSON.error)
+      }
+
+      const documents = apiResultJSON
+        .map(x => {
+          x.id = x.date
+          delete x.date
+
+          x.currencyA = currencyA.id
+          x.currencyB = currencyB.id
+
+          return x
+        })
+
+      // Upsert.
+      await store.getAdapter("rethinkdb").r.table('candlesticks').insert(documents, { conflict: "update" }).run();
+
+      return documents;
+    },
   },
 
   RootMutation: {
