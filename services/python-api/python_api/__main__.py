ed3ff55c1cd21c2d9a41351266ba9353d4bf1504
Tom Wieland <tom.wieland@gmail.com>
2019-12-06 20:03:25 +0100

    added keystone service

--- services/python-api/python_api/__main__.py
+++ services/python-api/python_api/__main__.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import logging
+from os import environ
 
-app = Flask(__name__)
+from aiohttp import web
 
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+PORT = int(environ['PYTHON_API_PORT_IN'])
 
 
-if __name__ == "__main__":
-    app.run()
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    web.run_app(app, port=PORT)
+
+    print('Started!')
